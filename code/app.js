31444bc33302f50946ab7b2b10784c7c84ff7908
Ati Takrami <ati.takrami@gmail.com>
2021-01-19 16:52:26 +0100

    add menu and make it responsive

---
+++ code/app.js
@@ -0,0 +1,8 @@
+function menuToggle() {
+  var menu = document.getElementById("main-menu-cnt");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
