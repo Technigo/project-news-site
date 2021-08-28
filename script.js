9aa6f2a9ad25e70ac708d28db11fda9f9f1b2b40
Your NameNamumariuchigit config --global user mariuchiqueiros@gmail.comgit statusgit config --global user.email “mariuchiqueiros”  git config --global user.name “Namumariuchi <you@example.com>
2021-08-28 12:21:47 +0200

    moved all pictures to the code folder

--- script.js
+++
@@ -1,5 +0,0 @@
-function showMenu(){
-    document.querySelector('.navigation').classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('hide');
-    document.querySelector('.close').classList.toggle('show');
-}
