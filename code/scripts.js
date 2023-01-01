dce3034f9bf9812bbc8e9095484c704d5446245d
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-01-01 22:44:10 +0100

    added foundations in html structure and css, now with media queries

---
+++ code/scripts.js
@@ -0,0 +1,6 @@
+let navigation = document.querySelector(".navigation");
+let navigationBtn = document.querySelector(".toggle-nav");
+
+function toggleNavigation() {
+  navigation.classList.toggle("toggle-visibility");
+}
