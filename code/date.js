b3d81838380ee45009d0890a62f0178a096b77b8
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-01-12 10:50:02 +0100

    Completed site

--- code/date.js
+++ code/date.js
@@ -1,44 +1,43 @@
 function updateDate() {
 
-let today = new Date();
+	let today = new Date();
 
-let dayName = today.getDay();
-let dayNum = today.getDate();
-let month = today.getMonth();
-let year = today.getFullYear();
+	let dayName = today.getDay();
+	let dayNum = today.getDate();
+	let month = today.getMonth();
+	let year = today.getFullYear();
 
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
 
-const dayWeek =[
-    "Sunday,", 
-    "Monday,",
-    "Tuesday,",
-    "Wednesday,",
-    "Thursday,",
-    "Friday,",
-    "Saturday,",    
-  ]
+	const dayWeek = [
+		"Sunday,",
+		"Monday,",
+		"Tuesday,",
+		"Wednesday,",
+		"Thursday,",
+		"Friday,",
+		"Saturday,",
+	]
 
-  const IDCollection = ["day","daynum","month","year"];
-  const val = [dayWeek[dayName], dayNum, months[month], year];
-  for (let i = 0; i < IDCollection.length; i++){
-    document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
-  }
+	const IDCollection = ["day", "daynum", "month", "year"];
+	const val = [dayWeek[dayName], dayNum, months[month], year];
+	for (let i = 0; i < IDCollection.length; i++) {
+		document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
+	}
 
 }
 
 updateDate();
-
