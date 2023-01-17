b820acf00c0d32a30e94288dcff37f7db3065524
Vera Sjunnesson <Vera@admins-mbp.mynet>
2023-01-17 23:54:31 +0100

    added some more animation

--- code/script.js
+++ code/script.js
@@ -6,3 +6,8 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
