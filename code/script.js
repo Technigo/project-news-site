344454dfd3de068547235a115fc48ec3d5738cab
Sofia Johansson <sofia@hopagarden.com>
2024-02-02 19:57:08 +0100

    Update - bug fix overflow on 668. Change h2 font size

--- code/script.js
+++ code/script.js
@@ -1,8 +1,3 @@
-/* ATTEMPT TO MOVE ONCLICK FROM HTML TO SCRIPT FILE
-const menuToggle = document.getElementById("menu-toggle");
-menuToggle.onclick = function () { toggleMenu() };
-*/
-
 function toggleMenu() {
     const menu = document.getElementById("menu-drawer");
     menu.classList.toggle("show");
