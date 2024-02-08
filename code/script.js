e139666981a595d933d19c91d948597376c3cbbf
Sofia Johansson <sofia@hopagarden.com>
2024-02-08 16:02:22 +0100

    Update - Switched to .addEventListener for menu toggle

--- code/script.js
+++ code/script.js
@@ -3,11 +3,11 @@ const menuIcon = document.querySelector("#menu-icon"); // Access menu burger ico
 const menuClose = document.querySelector("#menu-close"); // Access menu x icon
 
 // Toggle class 'show' to show or hide menu
-function toggleMenu() {
-    const menu = document.getElementById("menu-drawer");
-    menu.classList.toggle("show");
-    console.log(menu.classList)
-}
+const toggleMenu = () => {
+  const menu = document.getElementById("menu-drawer");
+  menu.classList.toggle("show");
+  console.log(menu.classList)
+};
 
-menuIcon.onclick = toggleMenu; // Call function when burger is clicked
-menuClose.onclick = toggleMenu; // Call function when close icon is clicked
+menuIcon.addEventListener("click", (toggleMenu)); // Call function when burger is clicked
+menuClose.addEventListener("click", (toggleMenu)); // Call function when close icon is clicked
