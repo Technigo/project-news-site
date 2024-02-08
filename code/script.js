20da0252bae679ed3c86a9313757437c87d68a45
Sofia Johansson <sofia@hopagarden.com>
2024-02-08 15:34:00 +0100

    Update - Switched to event handler for drawer menu

--- code/script.js
+++ code/script.js
@@ -1,4 +1,13 @@
+// DRAWER MENU
+const menuIcon = document.querySelector("#menu-icon"); // Access menu burger icon
+const menuClose = document.querySelector("#menu-close"); // Access menu x icon
+
+// Toggle class 'show' to show or hide menu
 function toggleMenu() {
     const menu = document.getElementById("menu-drawer");
     menu.classList.toggle("show");
+    console.log(menu.classList)
 }
+
+menuIcon.onclick = toggleMenu; // Call function when burger is clicked
+menuClose.onclick = toggleMenu; // Call function when close icon is clicked
